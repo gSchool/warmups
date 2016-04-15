5262385941c18fc4ae27257c2c9c2d284668ef36
Matt Williams <mwilliamsnov6@gmail.com>
2016-04-15 14:44:16 -0700

    cleaned up w1 warmups

--- w1/r-functions-warmup/r-functions-warmup.js
+++ w1/r-functions-warmup/r-functions-warmup.js
@@ -7,10 +7,10 @@
 // write a function declaration called addTwoNumbers that takes in two parameters
 // and returns the sum of those parameters 
 
-// write a function declaration called addManyNumbers that takes in two or three parameters
+// write a function declaration called addManyNumbers that takes in any number of parameters
 // and returns the sum of the input parameters 
 
-// INPUT THE LETTER OF THE CORRECT ANSWER INTO THE hoisting FUNCTION BELOW:
+// MUTIPLE CHOICE: INPUT THE LETTER OF THE CORRECT ANSWER INTO THE hoisting FUNCTION BELOW:
 // which one of these is hoisted to the top of the function scope?
 /*
     a: function iSeeALittleSilhouettoOfAMan() {
@@ -39,5 +39,6 @@ module.exports = {
   squareMe:squareMe,
   cubeMe:cubeMe,
   addTwoNumbers:addTwoNumbers,
+  addManyNumbers:addManyNumbers,
   hoisting:hoisting
 }
--- w1/w-objects-and-arrays-warmups/w-objects-and-arrays-warmup.js
+++ w1/w-objects-and-arrays-warmups/w-objects-and-arrays-warmup.js
@@ -4,26 +4,34 @@ var array = [];
 // put the number 5 in index 5 in arrayTwo listed below
 var arrayTwo = [0,0,0,0,0,0,0];
 
-// put 3 empty nested arrays in arrayThree listed below
+// put an empty array at index 0, 1, 2 in arrayThree listed below
 var arrayThree = [];
 
-// put a string with a length of 3, an empty object, and a number, into arrayFour listed below
+// put a string with a length of 3, an empty object, and a number (in that order) into arrayFour listed below
 var arrayFour = [];
 
 // put a key of foo and a value of "bar" into the object below
 var object = {};
 
-// put a key of nested and a value of an empty object into the object below
+// put a key of nestedObject and a value of an empty object into the object below
 var objectTwo = {};
 
-// put a key of superNested and a value of an object with a key of nested and a value of "data" into the object below
+// put a key of superNestedObject and a value of an object with a key of 
+// nestedObject and a value of "data" into the object below
 var objectThree = {};
 
-// put a key of arrayTwo and a value of arrayTwo (not a string), 
-// and a second key of arrayThree and a value of arrayThree (not a string) into the object below
+// put a key of arrayTwo and associate it with the variable arrayTwo (not a string), 
+// and a second key of arrayThree and associate it with the variable arrayThree (not a string) into the object below
 var objectFour = {};
 
 module.exports = {
+ attendance: "WORD GOES HERE",
  array: array,
- attendance: "WORD GOES HERE"
+ arrayTwo: arrayTwo,
+ arrayThree: arrayThree,
+ arrayFour: arrayFour,
+ object: object,
+ objectTwo: objectTwo,
+ objectThree: objectThree,
+ objectFour: objectFour
 }
