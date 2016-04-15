9009b62516b534d21dcacff2762941cdf6d1bc13
Matt Williams <mwilliamsnov6@gmail.com>
2016-04-15 11:35:19 -0700

    cleaned up master structure

---
+++ w1/t-using-attendance-word-only/t-using-attendance-word-only.js
@@ -0,0 +1,3 @@
+module.exports = {
+  attendance: "YOUR WORD HERE"
+}
---
+++ w1/w-objects-and-arrays-warmups/w-objects-and-arrays-warmup.js
@@ -0,0 +1,29 @@
+// starting at index 0, put numbers 1-5 in order in the array variable below.
+var array = [];
+
+// put the number 5 in index 5 in arrayTwo listed below
+var arrayTwo = [0,0,0,0,0,0,0];
+
+// put 3 empty nested arrays in arrayThree listed below
+var arrayThree = [];
+
+// put a string with a length of 3, an empty object, and a number, into arrayFour listed below
+var arrayFour = [];
+
+// put a key of foo and a value of "bar" into the object below
+var object = {};
+
+// put a key of nested and a value of an empty object into the object below
+var objectTwo = {};
+
+// put a key of superNested and a value of an object with a key of nested and a value of "data" into the object below
+var objectThree = {};
+
+// put a key of arrayTwo and a value of arrayTwo (not a string), 
+// and a second key of arrayThree and a value of arrayThree (not a string) into the object below
+var objectFour = {};
+
+module.exports = {
+ array: array,
+ attendance: "WORD GOES HERE"
+}
