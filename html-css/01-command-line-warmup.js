d5db9c28bbb5d5ccf6b10d4df3f8c30b70273f6c
Matt Williams <mwilliamsnov6@gmail.com>
2016-04-15 09:19:19 -0700

    updated command line warmup

---
+++ html-css/01-command-line-warmup.js
@@ -0,0 +1,67 @@
+function commandLineWarmup () {
+  var answerObj = {
+    /*  
+      Welcome to the morning warmup! 
+      please answer the following questions
+    */
+
+    /* In the shell: */
+    /* Q1: What command do I use to create a new file? */
+    
+    answer1: "your command here",
+
+
+    /* In the shell: */
+    /* Q2: How do you create a new directory? */
+
+    answer2: "your command here",
+
+    /* In the shell: */
+    /* Q3: What command do you type to go up one directory */
+
+    answer3: "your command here",
+
+    /* In the shell: */
+    /* Q4: What command do you type to see your current directory */
+    answer4: "your command here",
+
+    /* In the shell: */
+    /* Q5: what command do you type to chain multiple commands together? */
+    answer5: "your command here",
+
+    /* In the shell: */
+    /* Q6: What command do you type to remove a directory with child directories */
+    answer6: "your command here",
+
+    /* In the shell: */
+    /* Q7: What command do you type to list all hidden files? */
+    answer7: "your command here",
+
+    /* In the shell: */
+    /* Q8: What command do you type to locate the underlying path 
+       a shell command points to? */
+    answer8: "your command here",
+
+
+    /* In the shell: */
+    /* Q9: what command do you type to see the help files for a 
+       specific terminal command? */
+
+    answer9: "your command here",
+
+    /* In the shell: */
+    /* Q10: what command do you type to create text in the terminal and
+        add it to a new file called myText.txt in the present directory? */
+    answer10: "your command here"
+
+  };
+
+  return answerObj
+}
+
+console.log(commandLineWarmup())
+
+module.exports = {
+  commandLineWarmup: commandLineWarmup,
+  attendanceWord: "YOUR WORD HERE"
+}
