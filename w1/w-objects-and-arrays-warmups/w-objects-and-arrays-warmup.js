cc6c0bab088690cf37e82588e74f3758ce054a59
aaron <harms280@gmail.com>
2016-04-18 12:45:15 -0700

    add tests for warmup

--- w1/w-objects-and-arrays-warmups/w-objects-and-arrays-warmup.js
+++ w1/w-objects-and-arrays-warmups/w-objects-and-arrays-warmup.js
@@ -7,23 +7,31 @@ var arrayTwo = [0,0,0,0,0,0,0];
 // put 3 empty nested arrays in arrayThree listed below
 var arrayThree = [];
 
-// put a string with a length of 3, an empty object, and a number, into arrayFour listed below
+// Starting at index 0 put a string with a length of 3, an empty object,
+// and a number, into arrayFour listed below
 var arrayFour = [];
 
 // put a key of foo and a value of "bar" into the object below
 var object = {};
 
-// put a key of nested and a value of an empty object into the object below
+// put a key of nested with a value of an empty object into the object below
 var objectTwo = {};
 
 // put a key of superNested and a value of an object with a key of nested and a value of "data" into the object below
 var objectThree = {};
 
-// put a key of arrayTwo and a value of arrayTwo (not a string), 
+// put a key of arrayTwo and a value of arrayTwo (not a string),
 // and a second key of arrayThree and a value of arrayThree (not a string) into the object below
 var objectFour = {};
 
 module.exports = {
  array: array,
+ arrayTwo: arrayTwo,
+ arrayThree: arrayThree,
+ arrayFour: arrayFour,
+ object: object,
+ objectTwo: objectTwo,
+ objectThree: objectThree,
+ objectFour: objectFour,
  attendance: "WORD GOES HERE"
-}
+};
