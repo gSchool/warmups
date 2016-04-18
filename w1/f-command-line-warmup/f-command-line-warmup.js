47f9b3e099c20f7d412551d051cc7a1dd7ed48b5
aaron <harms280@gmail.com>
2016-04-18 15:18:24 -0700

    add test to friday warmup

--- w1/f-command-line-warmup/f-command-line-warmup.js
+++ w1/f-command-line-warmup/f-command-line-warmup.js
@@ -1,13 +1,13 @@
 function commandLineWarmup () {
   var answerObj = {
-    /*  
-      Welcome to the morning warmup! 
+    /*
+      Welcome to the morning warmup!
       please answer the following questions
     */
 
     /* In the shell: */
     /* Q1: What command do I use to create a new file? */
-    
+
     answer1: "your command here",
 
 
@@ -38,20 +38,20 @@ function commandLineWarmup () {
     answer7: "your command here",
 
     /* In the shell: */
-    /* Q8: What command do you type to locate the underlying path 
+    /* Q8: What command do you type to locate the underlying path
        a shell command points to? */
     answer8: "your command here",
 
 
     /* In the shell: */
-    /* Q9: what command do you type to see the help files for a 
+    /* Q9: what command do you type to see the help files for a
        specific terminal command? */
 
     answer9: "your command here",
 
     /* In the shell: */
-    /* Q10: what command do you type to create text in the terminal and
-        add it to a new file called myText.txt in the present directory? */
+    /* Q10: what command do you type to insert the text 'hello world' t in the terminal and
+        into a new file called myText.txt in the present directory? */
     answer10: "your command here"
 
   };
---
+++ w1/f-command-line-warmup/test/test-f-command-line-warmup.js
@@ -0,0 +1,62 @@
+var expect = require('chai').expect();
+var answers = require('../f-command-line-warmup');
+
+describe('Q1', function() {
+  it('should be touch', function() {
+      expect(answers.answer1).to.be.equal('touch');
+  });
+});
+
+describe('Q2', function() {
+  it('should be mkdir', function() {
+    expect(answers.answer2).to.be.equal('mkdir');
+  });
+});
+
+describe('Q3', function() {
+  it('should be cd ..', function() {
+    expect(answers.answer3).to.be.equal('cd ..');
+  });
+});
+
+describe('Q4', function() {
+  it('should be pwd', function() {
+    expect(answers.answer4).to.be.equal('pwd');
+  });
+});
+
+describe('Q5', function() {
+  it('should be |', function() {
+    expect(answers.answer5).to.be.equal('|');
+  });
+});
+
+describe('Q6', function() {
+  it('should be rm -rf', function() {
+    expect(answers.answer6).to.be.equal('rm -rf');
+  });
+});
+
+describe('Q7', function() {
+  it('should be ls -a', function() {
+    expect(answers.answer7).to.be.equal('ls -a');
+  });
+});
+
+describe('Q8', function() {
+  it('should be which', function() {
+    expect(answers.answer8).to.be.equal('which');
+  });
+});
+
+describe('Q9', function() {
+  it('should be man', function(){
+    expect(answers.answer9).to.be.equal('man');
+  });
+});
+
+describe('Q10', function() {
+  it('should be echo hello world > myText.txt', function() {
+    expect(answers.answer10).to.be.equal('echo hello world > myText.txt');
+  });
+});
