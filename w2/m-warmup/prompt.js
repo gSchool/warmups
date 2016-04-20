8bcb7a21106dbcfe74352db8391d26b898821d24
aaron <harms280@gmail.com>
2016-04-20 13:42:55 -0700

    create folders and prompt files

---
+++ w2/m-warmup/prompt.js
@@ -0,0 +1,11 @@
+function mondayWarmup () {
+  var answerObj = {
+    /*
+      Welcome to the morning warmup!
+      please answer the following questions
+    */
+
+    /* In the shell: */
+    /* Q1: ? */
+  };
+}
