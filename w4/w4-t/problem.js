bb04a9dac21038f00e4882010e0dea607bae6afc
aaron <harms280@gmail.com>
2016-04-29 14:45:09 -0700

    create tuesday warmup

---
+++ w4/w4-t/problem.js
@@ -0,0 +1,14 @@
+// Write a function that takes a string and returns an object 
+// with the letters of that string as keys and their value is how many times 
+// that letter occurred in the string. The counter function should not care about case.
+// The keys in the object should all be lowercase.
+// Ex: countLetters('aAbcdad') -> {a:3,b:1,c:1,d:2}
+
+function countLetters(s) {
+  //write solution here
+}
+
+module.exports = {
+  countLetters: countLetters,
+  attendance: 'Word Word Word Word Word Word'
+};
