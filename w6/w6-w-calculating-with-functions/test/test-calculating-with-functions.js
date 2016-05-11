3eec8ba94757d39d9165b7df6aadd455cd73f452
Matt Williams <mwilliamsnov6@gmail.com>
2016-05-11 15:19:50 -0700

    w6-w updated

--- w6/w6-w-calculating-with-functions/test/test-calculating-with-functions.js
+++ w6/w6-w-calculating-with-functions/test/test-calculating-with-functions.js
@@ -1,22 +1,22 @@
-var expect = require("chai").expect
-var allFunctions = require("../solution2")
+var expect = require("chai").expect;
+var allFunctions = require("../prompt");
 
-var zero = allFunctions.zero
-var one = allFunctions.one
-var two = allFunctions.two
-var three = allFunctions.three
-var four = allFunctions.four
-var five = allFunctions.five
-var six = allFunctions.six
-var seven = allFunctions.seven
-var eight = allFunctions.eight
-var nine = allFunctions.nine
-var plus = allFunctions.plus
-var minus = allFunctions.minus
-var times = allFunctions.times
-var dividedBy = allFunctions.dividedBy
+var zero = allFunctions.zero;
+var one = allFunctions.one;
+var two = allFunctions.two;
+var three = allFunctions.three;
+var four = allFunctions.four;
+var five = allFunctions.five;
+var six = allFunctions.six;
+var seven = allFunctions.seven;
+var eight = allFunctions.eight;
+var nine = allFunctions.nine;
+var plus = allFunctions.plus;
+var minus = allFunctions.minus;
+var times = allFunctions.times;
+var dividedBy = allFunctions.dividedBy;
 
-  console.log(five, "five")
+  console.log(five, "five");
 
 describe('calculating with function', function(){
   it('should return the correct calculation', function() {
@@ -26,6 +26,6 @@ describe('calculating with function', function(){
     expect(six(dividedBy(two()))).to.equal(3);
     expect(three(dividedBy(two()))).to.equal(1.5);
     expect(nine(times(nine()))).to.equal(81);
-  })
-})
+  });
+});
 
