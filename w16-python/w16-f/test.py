8be5a02d946add66b5604fa3e04bfbcb14182788
Matt Williams <mwilliamsnov6@gmail.com>
2016-07-18 09:00:34 -0700

    added python week 2 and w14 name correction

---
+++ w16-python/w16-f/test.py
@@ -0,0 +1,17 @@
+import unittest
+from prompt import *
+
+class Test(unittest.TestCase):
+
+  def tests_basic(self):
+    self.assertEqual(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+    self.assertEqual(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
+    self.assertEqual(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
+    self.assertEqual(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")
+    self.assertEqual(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")
+
+if __name__ == '__main__':
+  unittest.main()
+
+# To run the tests:        
+# python -m unittest test 
---
+++ w16-python/w16-t/solution.py
@@ -0,0 +1,14 @@
+# This time no story, no theory. The examples below show you 
+# how to write function accum:
+
+# Examples:
+
+# accum("abcd") --> "A-Bb-Ccc-Dddd"
+# accum("RqaEzty") --> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+# accum("cwAt") --> "C-Ww-Aaa-Tttt"
+
+# The parameter of accum is a string which includes only letters 
+# from a..z and A..Z.
+
+def accum(s):
+    return '-'.join(c.upper() + c.lower() * i for i, c in enumerate(s))
