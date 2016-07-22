ef3e4189b6eb308e7e9836e90131a3ee130c83aa
Matt Williams <mwilliamsnov6@gmail.com>
2016-07-22 11:53:08 -0700

    w17-t warmup added

---
+++ w17/w17-t-nested-sum-squares-of-numbers-in-list/prompt.js
@@ -0,0 +1,27 @@
+
+// Write a function that sums squares of numbers in list that may contain more lists
+
+// Example:
+
+// var l = [1,2,3]
+// SumSquares(l) == 14
+
+// var l = [[1,2],3]
+// SumSquares(l) == 14
+
+// var l = [[[[[[[[[1]]]]]]]]]
+// SumSquares(l) == 1
+
+// var l = [10,[[10],10],[10]]
+// SumSquares(l) == 400
+// Note: your solution must NOT modify the original list
+
+function sumSquares(l){
+
+}
+
+
+module.exports = {
+ sumSquares: sumSquares,
+ attendance: "much words" 
+}
