ad1b67327a7ac66597171a4b08d8450a3b67666d
Matt Williams <mwilliamsnov6@gmail.com>
2016-08-18 16:13:25 -0700

    w19-m

--- w2/w2-w-remove-dupes-string-warmup/prompt.js
+++ w2/w2-w-remove-dupes-string-warmup/prompt.js
@@ -1,10 +1,10 @@
-// Write a function named removeDupes which takes a string and removes 
-// all duplicate characters in the String. Here are some examples:  
+// Write a function named removeDupes which takes a string and removes
+// all duplicate characters in the String. Here are some examples:
 //   removeDupes('AABB'); // 'AB'
 //   removeDupes('AaAaBbBb'); // 'AaBb'
 //   removeDupes('cAtCaT'); // 'cAtCaT'
 
-// Write a function named removeDupesCI which takes a string and removes 
+// Write a function named removeDupesCI which takes a string and removes
 // all duplicate characters regardless of capitalization. Here are some examples:
 //   removeDupesCI('AABB'); // 'AB'
 //   removeDupesCI('AaAaBbBb'); // 'AB'
@@ -12,12 +12,12 @@
 
 
 function removeDupes(input){
-	
-} 
+
+}
 
 //CI for case insensitive
 function removeDupesCI(input){
-	
+
 }
 
 module.exports = {removeDupes:removeDupes,
