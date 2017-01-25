23145e6c345c12e028c66c534ce788a7d9913b20
Steve ODell <saodell@mac.com>
2017-01-25 15:32:02 -0700

    wednesday warmup

--- w08/w8-t-truncate/problem.js
+++ w08/w8-t-truncate/problem.js
@@ -5,14 +5,16 @@
 
 function truncateString(originalString, maxStringLength) {
   let stringLength = originalString.length
-
+// if max string is larger than actual string, do nothing
    if (maxStringLength >= stringLength) {
       console.log(originalString)
       return originalString
    }
+   // if max string is less than 3, don't count dot-dot-dot
       else if (maxStringLength <= 3) {
       return originalString.substring(0,maxStringLength) + "..."
    }
+   // otherwise, new string including dots = max string 
       else {
       return originalString.substring(0, (maxStringLength - 3)) + "..."
    }
