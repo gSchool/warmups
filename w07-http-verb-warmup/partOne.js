e40fcb5fe0ca2becaacabf03bf4708757a3d7b21
Hamid Aghdaee <hamid.aghdaee@galvanize.com>
2017-02-21 11:16:51 -0800

    reorganized

--- w07-http-verb-warmup/partOne.js
+++ w07-http-verb-warmup/partOne.js
@@ -1,24 +1,7 @@
 // 🐾🐾🐾 PART ONE 🐾🐾🐾
 
 var parseRequest = function (inputString) {
-  // yoyoyyosfasdfasdfasdfohhhh
-  var request = {};
-  request.headers = {};
-  var linesArr = inputString.split('\n')
-  var firstLine = linesArr[0].split(' ')
-  request.verb = firstLine[0]
-  request.path = firstLine[1]
-  request.protocol = firstLine[2]
-  for (var i = 1 ; i < linesArr.length ; i++) {
-    var currentLine = linesArr[i]
-    var currentKeyValueArray = currentLine.split(':');
-    var key = currentKeyValueArray[0];
-    var value = currentKeyValueArray[1];
-    request.headers[key] = value;
-  } // END FOR LOOP
-
-  return request
-
+  return;
 } // END FUNCTION
 
 module.exports = {
--- w07-http-verb-warmup/partTwo.js
+++ w07-http-verb-warmup/partTwo.js
@@ -1,42 +1,7 @@
 // 🐾🐾🐾 PART TWO 🐾🐾🐾
 
 var parseRequest = function (inputString) {
-
-  var request = {};
-  request.headers = {};
-  var linesArr = inputString.split('\n');
-  var firstLine = linesArr[0].split(' ');
-  request.verb = firstLine[0];
-  request.path = firstLine[1];
-  request.protocol = firstLine[2];
-
-  // grab all headers not including the body
-  var y = 1
-  while (linesArr[y+1]) {
-    var currentLine = linesArr[y]
-    var currentKeyValueArray = currentLine.split(':');
-    var key = currentKeyValueArray[0];
-    var value = currentKeyValueArray[1];
-    request.headers[key] = value;
-    y++
-  } // END FOR LOOP
-
-  if (request.verb === "POST") {
-    request.body = {};
-    var bodyString = linesArr[linesArr.length - 1];
-    var bodyStringPairsArray = bodyString.split('&');
-
-    for (var i = 0 ; i < bodyStringPairsArray.length ; i++) {
-      var keyValues = bodyStringPairsArray[i].split('=');
-      request.body[keyValues[0]] = keyValues[1]
-    } // END FOR LOOP
-
-    return request
-
-  } else {
-    return request
-  } // END IF
-
+  return;
 } // END FUNCTION
 
 module.exports = {
