80a391c9b1dc14eb8c066c82c72e03ae42c5cd42
Hamid Aghdaee <hamid.aghdaee@galvanize.com>
2017-03-31 16:24:52 -0700

    Update AddCardForm.js

--- react/day1-solution/src/components/AddCardForm.js
+++ react/day1-solution/src/components/AddCardForm.js
@@ -3,13 +3,21 @@ import React from 'react'
 export default class Component extends React.Component {
 
   constructor(props) {
-    super(props)
-    this.onSubmit = this.onSubmit.bind(this)
+    super(props);
+    this.state = {
+      title: ''
+    }
   }
 
-  onSubmit(e) {
+  onSubmit = (e) => {
     e.preventDefault()
-    console.log(this.refs.title.value)
+    console.log(this.state.title);
+  }
+
+  onTitleChange = (event) => {
+    this.setState({
+      title: event.target.value
+    });
   }
 
   render() {
@@ -17,7 +25,7 @@ export default class Component extends React.Component {
       <form className="well" onSubmit={this.onSubmit}>
         <fieldset className="form-group">
           <label htmlFor="title">Title</label>
-          <input type="text" ref="title" id="title" className="form-control" />
+          <input type="text" id="title" className="form-control" value={this.state.title} onChange={this.onTitleChange}/>
         </fieldset>
         <fieldset className="form-group">
           <button className="btn btn-success">Add Task</button>
