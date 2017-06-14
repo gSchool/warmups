1706ab9abbd0c5e25a50dfc1705827d4f419a4df
KatieAJenkins <kind2karma@gmail.com>
2017-06-14 11:24:21 -0600

    cleaned up code added logic

--- w07-http-verb-warmup/partOne.js
+++ w07-http-verb-warmup/partOne.js
@@ -5,26 +5,28 @@
 
 var parseRequest = function (inputString) {
   //1. Change this http request into a string (add \n) so we can break it into pieces - replace spaces with \n
-  //1. Need an empty request object to fill up
+  //2. Need an empty request object to fill up
       //var request = {};
-  //2. Need an empty request headers object
+  //3. Need an empty request headers object
       //var request.headers = {};
-  //3. Need to make each line into an array
+  //4. Need to make each line into an array
       //split on \n
-  //4. Split out GET request (1st array) by spaces
+  //5. Split out GET request (1st array) by spaces
       //split on ' '
       //creates new array with method, path, and protocol
-  //5. Take request object and set keys with values equal to the index of the value in the array:
+  //6. Take request object and set keys with values equal to the index of the value in the array:
     // - headers
     // - verb
     // - path
     // - protocol
-  //6. Loop through the last 3 arrays (var i =1 since we already took care of the first array/line) in lineArray
-  //7. Set a variable for current line
-  //8. Split the line on : to get key value pairs
-  //9. Set key from array[0]
-  //10. Set value from array[1]
-  //11. Add key to request.headers object and set value equal to value variable
+  //7. Loop through the last 3 arrays (var i =1 since we already took care of the first array/line) in lineArray
+  //8. Set a variable for current line
+  //9. Split the line on : to get key value pairs
+  //10. Set key from array[0]
+  //11. Set value from array[1]
+  //12. Add key to request.headers object and set value equal to value variable
+
+// console.log(inputString);
 
     var request = {};
     request.headers = {};
@@ -32,25 +34,28 @@ var parseRequest = function (inputString) {
     // console.log(lineArray);
     let firstLine = lineArray[0].split(' ');
     // console.log(firstLine);
+    // console.log(request);
     request.verb = firstLine[0];
     // console.log(request);
     request.path = firstLine[1];
     // console.log(request);
     request.protocol = firstLine[2];
+    // console.log(request);
 
     for (var i = 1; i < lineArray.length; i++){
       // console.log(lineArray[i]);
       var currentLine = lineArray[i];
-      console.log(currentLine);
+      // console.log(currentLine);
       var currentKeyValuePair = currentLine.split(':');
-      console.log(currentKeyValuePair);
+      // console.log(currentKeyValuePair);
       var key = currentKeyValuePair[0];
-      console.log(key);
+      // console.log('key', key);
       var value = currentKeyValuePair[1];
-      console.log(value);
+      // console.log('value', value);
+      console.log(request.headers);
       request.headers[key] = value;
-      console.log(request);
     }
+    // console.log(request);
   return request;
 } // END FUNCTION
 
