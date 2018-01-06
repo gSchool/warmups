ffd111fc7b6c04af3145e01853c68f80da6ce99d
Sean Helvey <sean.helvey@gSHaca671.local>
2018-01-06 14:09:32 -0700

    fetch and async fetch solution

---
+++ Fetch/main.js
@@ -0,0 +1,24 @@
+var fetch = require('node-fetch');
+
+function plainFetch() {
+  // this fetch grabs the boulder sub reddit
+  fetch('https://www.reddit.com/r/boulder.json').then(function(response) {
+    // the json method on the response object also returns a promise
+    return response.json();
+  }).then(function(json) {
+    // so any additional json processing must happen in another then
+    console.log(json.data.children[0]);
+  });
+}
+
+plainFetch();
+
+// How can we use async / await to rewrite the function above?
+
+async function asyncFetch() {
+  let response = await fetch('https://www.reddit.com/r/boulder.json')
+  let json = await response.json();
+  console.log(json.data.children[0]);
+}
+
+asyncFetch();
